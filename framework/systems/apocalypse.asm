82e807792009c7d5471f613b28510bc6b2e2b9d0
Matt Weber <matt@badecho.com>
2023-09-18 19:41:38 -0500

    Fixing previous brainfart (wrong jump instruction)

--- framework/systems/apocalypse.asm
+++ framework/systems/apocalypse.asm
@@ -131,7 +131,7 @@ executePlayerApocalypse:
     // Check if the damage being done is enough to warrant Apocalypse execution.
     mov rax,damageThreshold
     ucomiss xmm0,[rax]  
-    jle exitPlayerApocalypse
+    jbe exitPlayerApocalypse
     // If God Mode is disabled, then we apply the Apocalypse.
     cmp [playerGodMode],1
     jne applyApocalypse
